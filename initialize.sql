e36595290d38e4e4ba3f7bc46d859e9b5b982300
stepa <step4kap@gmail.com>
2023-09-13 22:14:31 +0300

    updated initialize.sql

--- initialize.sql
+++ initialize.sql
@@ -1,62 +1,52 @@
-DROP TABLE IF EXISTS ProjectAndRolesHistory, EIDToSalaryHistory, EIDToSalaryPolicy, EIDToProjectRoles, PIDToProjectRoles;
+DROP TABLE IF EXISTS EIDToSalaryHistory, EIDToSalaryPolicy, EIDToProjectRoles;
 
 DROP TABLE IF EXISTS EmployeeInfo, PayPolicy, Projects;
 
-DROP TYPE IF EXISTS policyType;
+DROP TYPE IF EXISTS policyType, projectRole;
 
-CREATE TYPE policyType  AS ENUM ('title', 'role', 'experience', 'additional');
+CREATE TYPE policyType  AS ENUM ('title', 'projectRole', 'experience', 'additional');
+CREATE TYPE projectRole AS ENUM ('supervisor', 'analyst', 'secretary', 'expert');
 
 CREATE TABLE EmployeeInfo (
-	EmployeeID int PRIMARY KEY,
+	EmployeeID integer PRIMARY KEY,
 	FullName varchar(100),
 	HomeAddress varchar(100),
 	Education varchar(100),
 	WorkExperience interval DAY,
-	JobTitle varchar(50),
+	JobTitle projectRole,
 	Salary integer
 );
 
 CREATE TABLE Projects (
-	ProjectID int PRIMARY KEY,
+	ProjectID integer PRIMARY KEY,
 	ProjectName varchar(50),
 	ProjectStart timestamp NOT NULL,
 	ProjectEnd timestamp
 );
 
 CREATE TABLE PayPolicy (
-	PolicyID int PRIMARY KEY,
+	PolicyID integer PRIMARY KEY,
 	PolicyType policyType,
 	PolicyDescription jsonb
 );
 
-CREATE TABLE ProjectAndRolesHistory (
-	EmployeeID int REFERENCES EmployeeInfo(EmployeeID),
-	PARHistory jsonb[]
-);
 
 CREATE TABLE EIDToSalaryHistory (
-	EmployeeID int REFERENCES EmployeeInfo(EmployeeID),
+	EmployeeID integer REFERENCES EmployeeInfo(EmployeeID),
 	SalaryHistory jsonb[]
 );
 
 CREATE TABLE EIDToSalaryPolicy (
-	EmployeeID int REFERENCES EmployeeInfo(EmployeeID),
-	PolicyID int[]
+	EmployeeID integer REFERENCES EmployeeInfo(EmployeeID),
+	PolicyID integer REFERENCES PayPolicy(PolicyID)
 );
 
 CREATE TABLE EIDToProjectRoles (
-	EmployeeID int REFERENCES EmployeeInfo(EmployeeID),
-	ProjectID int REFERENCES Projects(ProjectID),
-	ProjectRole varchar(50),
+	EmployeeID integer REFERENCES EmployeeInfo(EmployeeID),
+	ProjectID integer REFERENCES Projects(ProjectID),
+	ProjectRole projectRole,
 	PRIMARY KEY(EmployeeID, ProjectID)
 );
-
-CREATE TABLE PIDToProjecTRoles (
-	ProjectID int REFERENCES Projects(ProjectID),
-	ProjectRoles varchar(50)[]
-);
-
-
 
 
 
