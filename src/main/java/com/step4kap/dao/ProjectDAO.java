b8dd6174d8a13a1cab4d8ff67fc7a0f8911ed9ab
Qffsky <step4kap@gmail.com>
2023-09-29 13:57:55 +0300

    FIX: type in html

--- src/main/java/com/step4kap/dao/ProjectDAO.java
+++ src/main/java/com/step4kap/dao/ProjectDAO.java
@@ -1,9 +1,14 @@
 package com.step4kap.dao;
 
+import com.step4kap.db.EmployeeInfo;
 import com.step4kap.db.Project;
+
+import java.util.List;
 
 public class ProjectDAO extends CommonDAO<Project, Integer> {
     public ProjectDAO() {
         super(Project.class);
     }
+
+//    List<EmployeeInfo> getEmployeeInfoByProjectId()
 }
